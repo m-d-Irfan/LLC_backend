5436a7dd10b37c1550903c826d0108ab72f70c4c
CodesBreakerS <monzurul.islam2022@gmail.com>
2025-10-21 03:05:47 +0600

    the apps has been added

--- llc/llc/settings.py
+++ llc/llc/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'catalog',
+    'core',
+    'exams',
 ]
 
 MIDDLEWARE = [
