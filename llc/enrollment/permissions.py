938dcbe7ac1c8e2bd4a1c2ded4a62a8b344e087a
m-d-Irfan <monzurul.islam2022@gmail.com>
2026-04-17 21:27:14 +0600

    fixing files

--- llc/enrollment/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsStudent(BasePermission):
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        else:
-            return request.user.is_student
